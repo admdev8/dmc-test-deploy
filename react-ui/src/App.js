f1d52d223f43968d4f16557b0d46dab0db04cb4f
jusrockinout <gary.newton@ymail.com>
2017-08-08 13:49:06 -0400

    Attempting to deploy react site

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,55 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './css/grayscale.css';
+import './css/grayscale.min.css';
+// import $ from 'jquery';
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import About from './Components/About';
+import Search from './Components/Search';
+import Map from './Components/Map';
+import Contact from './Components/Contact';
+import Register from './Components/Register';
+import Footer from './Components/Footer';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: null,
-      fetching: true
-    };
-  }
-
-  componentDidMount() {
-    fetch('/api')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        this.setState({
-          message: json.message,
-          fetching: false
-        });
-      }).catch(e => {
-        this.setState({
-          message: `API call failed: ${e}`,
-          fetching: false
-        });
-      })
-  }
-
   render() {
-    return (
+    console.log("render: True");
+     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          {'This is '}
-          <a href="https://github.com/mars/heroku-cra-node">
-            {'create-react-app with a custom Node/Express server'}
-          </a><br/>
-        </p>
-        <p className="App-intro">
-          {this.state.fetching
-            ? 'Fetching message from API'
-            : this.state.message}
-        </p>
+        <Header />
+        <Navbar />
+        <About />
+        <Search />
+        <Map />
+        <Contact />
+        <Register />
+        <Footer />
       </div>
     );
   }
